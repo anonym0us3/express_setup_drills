866371d122d6100c38b29844403e6b03b8cd6f2e
anonym0us3 <nathan.shayevich@gmail.com>
2015-12-02 20:34:39 -0800

    page/project 1 finished. Also added views & index.html pages to have a more complete file tree structure

--- aproject/server.js
+++ aproject/server.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+var taquerias = [
+	{ name: "Ocean Taqueria" },
+	{ name: "Taqueria La Cumbre"},
+	{ name: "Little Chihuahua"},
+	{ name: "Gordo Taqueria"}
+];
+
+app.get('/taquerias', function (req, res) {
+	res.json(taquerias);
 });
 
 var server = app.listen(3000);
