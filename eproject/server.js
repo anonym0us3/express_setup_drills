629ff9e4218126599ea1ce5b5949404d49dd3fb8
anonym0us3 <nathan.shayevich@gmail.com>
2015-12-02 21:52:59 -0800

    finished the HW, last project uploadifying. Yes, I really *did* completely run out of ideas

--- eproject/server.js
+++ eproject/server.js
@@ -2,6 +2,24 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
+var randomThingsAroundMe = [
+	"Desks",
+	"Chairs",
+	"Cleaning crew",
+	"Coffee machine",
+	"Sleeping pods",
+	"Fake cacti",
+	"Ceiling lights\' reflections in the windows"
+];
 
+app.get('/randomThingsAroundMe', function (req, res) {
+	res.send(randomThingsAroundMe);
+});
+
+var numberz = [1, 2, 3, 7, 13, 42, 31337];
+
+app.get('/numberz', function (req, res) {
+	res.send(numberz);
+});
 
 var server = app.listen(3000);
